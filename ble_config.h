ed5ccd8c8f64442a1a86c1f1ddef140dab7a68fe
lumio-backup <lumio_backup@uniqus.com>
2025-12-08 20:45:46 +0530

    changed timestamp to milllis in config data

--- ble_config.h
+++ ble_config.h
@@ -65,10 +65,6 @@ private:
         currentSSID = "Not configured";
       }
       
-      // Get timestamp
-      char timestamp[20];
-      TimeSync::getCurrentTimeString(timestamp, sizeof(timestamp));
-      
       // Get battery level
       int batteryLevel = BatteryMonitor::getBatteryPercentage();
       
@@ -77,8 +73,8 @@ private:
       
       char jsonBuffer[256];
       snprintf(jsonBuffer, sizeof(jsonBuffer), 
-               "{\"device_id\":\"%s\",\"device_version\":\"%s\",\"timestamp\":\"%s\",\"battery\":%d,\"currentSSID\":\"%s\",\"debug_mode\":%d,\"cycle_time\":%d}",
-               devId, devVer, timestamp, batteryLevel, currentSSID.c_str(), NVSConfig::getDebugMode(), NVSConfig::getCycleTime());
+               "{\"device_id\":\"%s\",\"device_version\":\"%s\",\"timestamp\":\"%llu\",\"battery\":%d,\"currentSSID\":\"%s\",\"debug_mode\":%d,\"cycle_time\":%d}",
+               devId, devVer, TimeSync::getCurrentTimeMillis();, batteryLevel, currentSSID.c_str(), NVSConfig::getDebugMode(), NVSConfig::getCycleTime());
       
       // Send JSON data via Current SSID Characteristic
       if (pCurrentSSIDCharacteristic != nullptr) {
