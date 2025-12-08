d93e74557085c351d34911c8ab08706ffda00c76
lumio-backup <lumio_backup@uniqus.com>
2025-12-08 20:44:01 +0530

    changed url, changed voltage to separate from battery

--- ble_config.h
+++ ble_config.h
@@ -65,20 +65,17 @@ private:
         currentSSID = "Not configured";
       }
       
-      // Get timestamp
-      char timestamp[20];
-      TimeSync::getCurrentTimeString(timestamp, sizeof(timestamp));
-      
       // Get battery level
-      String batteryLevel = BatteryMonitor::getBatteryPercentageV();
+      float batteryVoltage = BatteryMonitor::readBatteryVoltage();
+      int batteryLevel = BatteryMonitor::getBatteryPercentageV(batteryVoltage);
       
       const char* devId = (deviceId != nullptr) ? deviceId : "Unknown";
       const char* devVer = (deviceVersion != nullptr) ? deviceVersion : "v0.0.0";
       
       char jsonBuffer[256];
       snprintf(jsonBuffer, sizeof(jsonBuffer), 
-               "{\"device_id\":\"%s\",\"device_version\":\"%s\",\"timestamp\":\"%s\",\"battery\":%s,\"currentSSID\":\"%s\",\"debug_mode\":%d,\"cycle_time\":%d}",
-               devId, devVer, timestamp, batteryLevel, currentSSID.c_str(), NVSConfig::getDebugMode(), NVSConfig::getCycleTime());
+               "{\"device_id\":\"%s\",\"device_version\":\"%s\",\"timestamp\":\"%llu\",\"battery\":%d,\"voltage\":%.3f,\"currentSSID\":\"%s\",\"debug_mode\":%d,\"cycle_time\":%d}",
+               devId, devVer, TimeSync::getCurrentTimeMillis(), batteryLevel, currentSSID.c_str(), NVSConfig::getDebugMode(), NVSConfig::getCycleTime());
       
       // Send JSON data via Current SSID Characteristic
       if (pCurrentSSIDCharacteristic != nullptr) {
