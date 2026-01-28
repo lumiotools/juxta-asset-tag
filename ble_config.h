a8a7ff6da03d926c3abd94e7a4fb7d05d9ba6b6b
DawoodKhatri <dawoodkhatri18@gmail.com>
2026-01-28 23:54:36 +0530

    ble config patch

--- ble_config.h
+++ ble_config.h
@@ -605,12 +605,11 @@ public:
     // Set advertising parameters for better discoverability
     // 2 = BLE_GAP_CONN_MODE_UND (undirected connectable advertising)
     pAdvertising->setConnectableMode(2);
-    pAdvertising->setMinInterval(32);   // 20ms (units of 0.625ms)
-    pAdvertising->setMaxInterval(160);  // 100ms (units of 0.625ms)
+    pAdvertising->setMinInterval(244);   
+    pAdvertising->setMaxInterval(252);
     pAdvertising->enableScanResponse(true);
     
     // Add service UUID to advertising data
-    pAdvertising->addServiceUUID(SERVICE_UUID);
     
     // Set device name in advertising data (important for discoverability)
     // Note: In NimBLE 2.x, device name must be explicitly set
