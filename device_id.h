1585b76005cc5eabdfc865a6f3cd4d7c603fe6da
DawoodKhatri <dawoodkhatri18@gmail.com>
2026-02-06 11:32:26 +0530

    updated device id and gps bauds

--- device_id.h
+++ device_id.h
@@ -9,7 +9,7 @@ public:
   // Get MAC address as string (format: AA:BB:CC:DD:EE:FF)
   static void getMACAddress(char* buffer, size_t bufSize) {
     byte mac[6];
-    esp_read_mac(mac, ESP_MAC_WIFI_STA);
+    esp_read_mac(mac, ESP_MAC_EFUSE_FACTORY);
     snprintf(buffer, bufSize, "%02X:%02X:%02X:%02X:%02X:%02X",
              mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
   }
--- gps_sensor.h
+++ gps_sensor.h
@@ -102,7 +102,7 @@ private:
   // Detect which baud rate is working (9600 or 115200)
   // Returns the baud rate that receives valid NMEA data, or 0 if neither works
   uint32_t detectBaudRate() {
-    uint32_t baudRates[] = {4800,9600,19200,38400,57600,115200};
+    uint32_t baudRates[] = {115200};
     const int detectionTimeout = 1000; // 1 second to detect data
     const int minValidChars = 10; // Minimum characters to consider valid data
     
